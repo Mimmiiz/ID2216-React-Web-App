335d9f1ff7e08c9ce69e357493abdffa57f69516
Oliver Schwalbe Lehtihet <sl.oliver@proton.me>
2023-02-08 11:02:36 +0100

    Added skeleton for a dynamic Services page. Needs State from Main page

--- src/Services.js
+++ src/Services.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import Navbar from './Navbar';
+import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+import './styles/Services.css'
 
 /* Shows a list of all the services provided for a specific category, e.g., Salon */
 function Services() {
+
+    const location = useLocation();
+    console.log(location);
+    console.log(location.state.subcategories[0]);
+
+    // Idea: Go through the length of the 'subcategories' array in the for loop and add a button for each element. Send state to service-persons page
+    const rows = [];
+    for (let i = 0; i < 4; i++) {
+        rows.push(
+            <Link key={i} className='services-link' to="/service-persons">
+            <button className="services-btn" >Link Button</button>
+            </Link>
+        )
+    }
+
     return (
-        <div>
-            <h1>List of provided services.</h1>
+        <div className="Services">
+            {rows}
+            <Navbar/>
         </div>
     )
 }
 
 export default Services;
+

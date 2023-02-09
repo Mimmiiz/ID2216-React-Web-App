961c2601181a10f4d9702bdfca25b2ed0025f669
Oliver Schwalbe Lehtihet <sl.oliver@proton.me>
2023-02-09 04:10:58 +0100

    Render dynamically by using state from the home page. Re-directs to /service-persons with category as state. Fixes #1

--- src/Services.js
+++ src/Services.js
@@ -4,19 +4,23 @@ import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import './styles/Services.css'
 
+import { Navigate } from 'react-router-dom';
+
 /* Shows a list of all the services provided for a specific category, e.g., Salon */
 function Services() {
 
     const location = useLocation();
-    console.log(location);
-    console.log(location.state.subcategories[0]);
 
-    // Idea: Go through the length of the 'subcategories' array in the for loop and add a button for each element. Send state to service-persons page
+    if (location.state == null) {
+        console.log("Error: State is null, select one of the buttons on the main page");
+        return <>{<Navigate to="/" />}</>;
+    }
+
     const rows = [];
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < location.state.subcategories.length; i++) {
         rows.push(
-            <Link key={i} className='services-link' to="/service-persons">
-            <button className="services-btn" >Link Button</button>
+            <Link key={i} className='services-link' to="/service-persons" state = {{type: location.state.subcategories[i]}}>
+                <button className="services-btn" >{location.state.subcategories[i]}</button>
             </Link>
         )
     }
